0b7666bad5840ba51f1cb4a9cd36992c9d99de40
JulieChoJulie <h32cho@gmail.com>
2019-09-23 15:45:33 -0700

    todotemplate added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
     return (
         <div>
-          Hello
+          <TodoTemplate>Let's make todo app.</TodoTemplate>
         </div>
     );
 };
---
+++ src/components/TodoTemplate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TodoTemplate.scss'
+
+const TodoTemplate = ({ children }) => {
+    return (
+        <div className="TodoTemplate">
+            <div className="title">Scheduler</div>
+            <div className="content">{children}</div>
+
+        </div>
+    );
+};
+
+export default TodoTemplate;

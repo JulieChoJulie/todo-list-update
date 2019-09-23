dbae904992c800285fae9ecd06d246f44d3d7bad
JulieChoJulie <h32cho@gmail.com>
2019-09-23 16:32:23 -0700

    todoitems rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
+    const [todos, setTodos] = useState([
+        {id: 1, text: 'do chem hw', checked: true},
+        {id: 2, text: 'trim nails', checked: false},
+        {id: 3, text: 'prep tmrw lunch', checked: false},
+    ]);
+
     return (
         <div>
           <TodoTemplate>
               <TodoInsert />
-              <TodoList />
+              <TodoList todos={todos}/>
           </TodoTemplate>
 
         </div>
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -5,14 +5,16 @@ import {
     MdCheckBox,
     MdRemoveCircleOutline,
 } from "react-icons/md";
+import cn from 'classnames';
 
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo }) => {
+    const { text, checked } = todo;
     return (
         <div className="TodoListItem">
-            <div className="checkbox">
-                <MdCheckBoxOutlineBlank/>
-                <div className="text">hello</div>
+            <div className={cn('checkbox', { checked })}>
+                {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
+                <div className="text">{text}</div>
             </div>
             <div className="remove">
                 <MdRemoveCircleOutline/>

1f3f6f0197361eb72c2478d03723cd8c59b4e4eb
JulieChoJulie <h32cho@gmail.com>
2019-09-24 13:14:43 -0700

    react-virtualized added

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -8,17 +8,19 @@ import {
 import cn from 'classnames';
 
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
     const { text, checked } = todo;
     return (
-        <div className="TodoListItem">
-            <div className={cn('checkbox', { checked })} onClick={() => onToggle(todo.id)}>
-                {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
-                <div className="text">{text}</div>
-            </div>
-            <div
-                className="remove" onClick={() => onRemove(todo.id)}>
-                <MdRemoveCircleOutline/>
+        <div className="TodoListItem-virtualized" style={style}>
+            <div className="TodoListItem">
+                <div className={cn('checkbox', { checked })} onClick={() => onToggle(todo.id)}>
+                    {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
+                    <div className="text">{text}</div>
+                </div>
+                <div
+                    className="remove" onClick={() => onRemove(todo.id)}>
+                    <MdRemoveCircleOutline/>
+                </div>
             </div>
         </div>
     );

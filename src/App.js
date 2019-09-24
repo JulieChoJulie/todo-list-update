9bce8c108a68c32c9d67f9f0af7e0b1cca2616f8
JulieChoJulie <h32cho@gmail.com>
2019-09-24 12:40:47 -0700

    useState update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+function createBulkTodos() {
+    const array = [];
+    for (let i = 1; i <= 2500; i++) {
+        array.push({
+            id: i,
+            text: `todo ${i}`,
+            checked: false,
+        });
+    }
+    return array;
+}
+
 const App = () => {
-    const [todos, setTodos] = useState([
-        {id: 1, text: 'do chem hw', checked: true},
-        {id: 2, text: 'trim nails', checked: false},
-        {id: 3, text: 'prep tmrw lunch', checked: false},
-    ]);
+    // const [todos, setTodos] = useState([
+    //     {id: 1, text: 'do chem hw', checked: true},
+    //     {id: 2, text: 'trim nails', checked: false},
+    //     {id: 3, text: 'prep tmrw lunch', checked: false},
+    // ]);
+    const [todos, setTodos] = useState(createBulkTodos);
 
     const nextId = useRef(4);
 
@@ -18,22 +31,21 @@ const App = () => {
             text,
             checked: false,
         };
-        setTodos(todos.concat(todo));
+        setTodos(todos => todos.concat(todo));
         nextId.current += 1;
-    }, [todos]);
+    }, []);
 
     const onRemove = useCallback(id => {
-        const editedTodos = todos.filter(todo => todo.id !== id);
-        setTodos(editedTodos);
-    }, [todos]);
+        setTodos(todos => todos.filter(todo => todo.id !== id));
+    }, []);
 
     const onToggle = useCallback(id => {
-        setTodos(
+        setTodos(todos =>
             todos.map(todo =>
                 todo.id === id ? {...todo, checked: !todo.checked} : todo
             )
         );
-    }, [todos]);
+    }, []);
 
     return (
         <div>
